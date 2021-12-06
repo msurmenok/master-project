91e9369198faaee3184ff7003c849709b759481a
msurmenok <mariasurmenok@gmail.com>
2021-12-06 08:16:54 -0800

    exp9 for slow traffic

--- source/single_experiment_main.py
+++ source/single_experiment_main.py
@@ -251,7 +251,7 @@ def run_single_experiment(iteration, algorithms, config, simulationDuration):
 
     # average = 'average2'
 
-    current_setting = weak_fog_devices  # third experiment
+    current_setting = slow_traffic  # third experiment
 
     folder_results = 'results_' + current_setting + '/current/results_' + config['scenario'] + '_' + str(iteration)
     folder_data = 'data_' + current_setting + '/' + 'data_' + config['scenario'] + '_' + str(iteration)
