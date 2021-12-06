90fec806b621e9e2a3408aab29c4fa2ce743de68
msurmenok <mariasurmenok@gmail.com>
2021-12-06 08:15:01 -0800

    weak fog devices experiment

--- source/single_experiment_main.py
+++ source/single_experiment_main.py
@@ -251,7 +251,7 @@ def run_single_experiment(iteration, algorithms, config, simulationDuration):
 
     # average = 'average2'
 
-    current_setting = high_traffic  # third experiment
+    current_setting = weak_fog_devices  # third experiment
 
     folder_results = 'results_' + current_setting + '/current/results_' + config['scenario'] + '_' + str(iteration)
     folder_data = 'data_' + current_setting + '/' + 'data_' + config['scenario'] + '_' + str(iteration)
